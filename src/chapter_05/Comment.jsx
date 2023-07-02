39b7a3392a8a7b0e62ceccaf5efd1bc7af54d93b
ebcode2021 <ebcode2021@gmail.com>
2023-07-02 20:12:34 +0900

    [add] first component

---
+++ src/chapter_05/Comment.jsx
@@ -0,0 +1,9 @@
+function	Comment(props) {
+	return (
+		<div>
+			<h1> 내가 만든 첫 컴포넌트!</h1>
+		</div>
+	);
+}
+
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -5,18 +5,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
 
+
+// chapter_05
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>
+);
 
 // chapter_04
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
+// setInterval(() => {
+//   root.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>
+//   );
+// }, 1000);
 
 // chapter_03
 // const root = ReactDOM.createRoot(document.getElementById('root'));
