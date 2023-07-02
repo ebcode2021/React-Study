3d3869adee41d578db07e7e0a6a8fabd00725257
ebcode2021 <ebcode2021@gmail.com>
2023-07-02 20:41:57 +0900

    [add] change jsx css to css file

--- src/chapter_05/Comment.jsx
+++ src/chapter_05/Comment.jsx
@@ -1,9 +1,78 @@
+import "./styles.css"
+
 function	Comment(props) {
 	return (
-		<div>
-			<h1> 내가 만든 첫 컴포넌트!</h1>
+		<div className="wrapper">
+			<div className="imageContainer">
+				<img 
+					className="image"
+					src="https://img.freepik.com/premium-photo/raccoon-9-months-procyon-lotor-isolated_191971-11672.jpg" 
+				/>
+			</div>
+			<div className="contentContainer">
+				<span className="nameText">
+					{props.name}
+				</span>
+				<span className="commentText">
+					{props.comment}
+				</span>
+			</div>
 		</div>
 	);
 }
 
 export default Comment;
+
+// remove jsx css style
+// function	Comment(props) {
+// 	return (
+// 		<div style={styles.wrapper}>
+// 			<div style={styles.imageContainer}>
+// 				<img src="https://img.freepik.com/premium-photo/raccoon-9-months-procyon-lotor-isolated_191971-11672.jpg" 
+// 					style={styles.image}/>
+// 			</div>
+// 			<div style={styles.contentContainer}>
+// 				<span style={styles.nameText}>
+// 					은비
+// 				</span>
+// 				<span style={styles.commentText}>
+// 					내가 만든 컴포넌트
+// 				</span>
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// export default Comment;
+
+// const styles = {
+//     wrapper: {
+//         margin: 8,
+//         padding: 8,
+//         display: "flex",
+//         flexDirection: "row",
+//         border: "1px solid grey",
+//         borderRadius: 16,
+//     },
+//     imageContainer: {},
+//     image: {
+//         width: 50,
+//         height: 50,
+//         borderRadius: 25,
+//     },
+//     contentContainer: {
+//         marginLeft: 8,
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "center",
+//     },
+//     nameText: {
+//         color: "black",
+//         fontSize: 16,
+//         fontWeight: "bold",
+//     },
+//     commentText: {
+//         color: "black",
+//         fontSize: 16,
+//     },
+// };
--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,9 +1,28 @@
 import Comment from "./Comment";
 
+const comments = [
+	{
+		name: "손은비",
+		comment: "안녕하세요, 은비입니다.",
+	},
+	{
+		name: "김지수",
+		comment: "안녕하세요, 지수입니다.",
+	},
+	{
+		name: "배고파요",
+		comment: "안녕하세요, 식단중입니다.",
+	}
+];
+
 function CommentList(props) {
 	return (
 		<div>
-			<Comment/>
+			{comments.map((comment) => {
+				return (
+					<Comment name={comment.name} comment={comment.comment}/>
+				);
+			})};
 		</div>
 	);
 }
