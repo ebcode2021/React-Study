7d5bbedb900359a4e6b35b6111b4b02a69de7ae3
ebcode2021 <ebcode2021@gmail.com>
2023-08-06 12:51:22 +0900

    [mv] file

--- First_Encounter_With_React/src/chapter_12/Calculator.jsx
+++ First_Encounter_With_React/src/chapter_12/Calculator.jsx
@@ -1,10 +1,11 @@
-import TemperatureInput from "./TemperatureInput";
+import TemperatureInput from './TemperatureInput';
+import React, { useState } from 'react';
 
 function BoilingVerdict(props) {
 	if (props.celsius >= 100) {
-		return <p>물 끓는중..</p>
+		return <p>물 끓는중..</p>;
 	}
-	return <p>물 안끓는중..</p>
+	return <p>물 안끓는중..</p>;
 }
 
 function toCelsius(fahrenheit) {
@@ -17,46 +18,37 @@ function toFahrenheit(celsius) {
 
 function tryConvert(temperature, convert) {
 	const input = parseFloat(temperature);
-	if (Number.isNaN(input))
-			return "";
-	
+	if (Number.isNaN(input)) {
+		return '';
+	}
+
 	const output = convert(input);
 	const rounded = Math.round(output * 1000) / 1000;
-	
+
 	return rounded.toString();
 }
 
 function Calculator(props) {
-	const [temperature, setTemperature] = useState("");
-	const [scale, setScale] = useState("c");
+	const [temperature, setTemperature] = useState('');
+	const [scale, setScale] = useState('c');
 
 	const handleCelsiusChange = (temperature) => {
 		setTemperature(temperature);
-		setScale("c");
+		setScale('c');
 	};
 
 	const handleFahrenheitChange = (temperature) => {
 		setTemperature(temperature);
-		setScale("f");
+		setScale('f');
 	};
 
-	const celsius =
-		scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
-	const fahrenheit =
-		scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
+	const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+	const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
 
 	return (
 		<div>
-			<TemperatureInput
-				scale="c"
-				temperature={celsius}
-				onTemperatureChange={handleCelsiusChange}
-			/>
-			<TemperatureInput
-				scale="f"
-				temperature={celsius}
-				onTemperatureChange={handleCelsiusChange}
-			/>
+			<TemperatureInput scale="c" temperature={celsius} onTemperatureChange={handleCelsiusChange} />
+			<TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={handleFahrenheitChange} />
 			<BoilingVerdict celsius={parseFloat(celsius)} />
 		</div>
 	);
---
+++ README.md
@@ -0,0 +1,35 @@
+React를 공부하면서 기록한 <b>소스코드</b> 및 <b>노션 페이지</b> 입니다.
+
+---
+
+## 1. First_Encounter_With_Reat
+
+책 `"소플의 처음 만난 리액트"`의 ch3 - ch15의 소스코드가 적혀있습니다.
+
+    [keyword]
+    JSX, element, component, props, state, hook, event-handling, list, form, containment, inheritance, context
+
+## 2. Mini_Blog
+
+서버가 존재하지 않아 실제 블로그처럼 작동하지는 않지만, 블로그의 <b>뷰</b>와 <b>기능</b>을 리액트 컴포넌트로 만들었습니다.
+
+    [function]
+    - 글 목록 보기 기능
+    - 글, 댓글 보기 기능
+    - 글, 댓글 작성 기능
+
+## 3. Movie_Web_Site
+
+강의 `"ReactJS로 영화 웹 서비스 만들기"`를 수강하면서 영화 소개 웹 사이트를 클론 코딩하였습니다.
+
+## 4. Study_With_Lecture
+
+강의 `"ReactJS로 영화 웹 서비스 만들기"`의 소스 코드가 적혀있습니다.
+
+## 5. Notion Page
+
+소스 코드 외의 공부 기록이 담겨있는 노션 페이지 입니다.
+
+## 6. Reference
+
+[강의] https://nomadcoders.co/react-for-beginners
